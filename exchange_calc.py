12e122cbc04f36805bee5b3263794feb58e30714
Suvam <fmahuyac@gmail.com>
2023-02-09 13:25:25 +0100

    correcting version control errors

--- exchange_calc.py
+++ exchange_calc.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-def currency(input_currency, output_currency, amount):
+def rate_calc(input_currency, output_currency, amount=1):
     url = f"https://www.x-rates.com/calculator/?from={input_currency}&to={output_currency}&amount={amount}"
     content = requests.get(url).text
     soup = BeautifulSoup(content, 'html.parser')
     currency = soup.find('span', class_ = "ccOutputRslt").get_text()
-    print(currency)
+    rate = float(currency.split(' ')[0])
+    return rate
 
 if __name__ == '__main__':
-    currency('EUR', 'INR', '10')
+    currency('EUR', 'INR')
 
--- first_file.py
+++
@@ -1,1 +0,0 @@
-print("This works in Local Machine ")
